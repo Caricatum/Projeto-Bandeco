07b07fb9c4f5d500b48643507247a54fbcb1a38c
Tortoli <guitortoli7@gmail.com>
2026-06-17 20:31:41 -0300

    Desktop - casca

---
+++ Codigo/Desktop/ProjetoBandejao/ProjetoBandejao/Forms/CadastroFuncionarioForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetoBandejao.Models;
+using ProjetoBandejao.Services;
+
+namespace ProjetoBandejao.Forms
+{
+    public partial class CadastroFuncionarioForm : Form
+    {
+        public CadastroFuncionarioForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.Trim() == "" ||
+                 txtEmail.Text.Trim() == "" ||
+                 txtSenha.Text.Trim() == "")
+            {
+                MessageBox.Show(
+                    "Preencha todos os campos.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            Usuario usuario = new Usuario
+            {
+                Nome = txtNome.Text.Trim(),
+                Login = txtEmail.Text.Trim(),
+                Senha = txtSenha.Text,
+                Funcionario = chkAdministrador.Checked
+            };
+
+            UsuarioService service = new UsuarioService();
+
+            bool sucesso = service.Cadastrar(usuario);
+
+            if (sucesso)
+            {
+                MessageBox.Show(
+                          "Funcionário cadastrado com sucesso!\nAgora confirme o e-mail.",
+                          "Sucesso",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+
+                ConfirmarCodigoForm tela =
+                    new ConfirmarCodigoForm(
+                        txtEmail.Text.Trim());
+
+                tela.ShowDialog();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Erro ao cadastrar funcionário.",
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void CadastroFuncionarioForm_Load(object sender, EventArgs e)
+        {
+            this.Shown += (s, e) =>
+            {
+                ActiveControl = null;
+            };
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Codigo/Desktop/ProjetoBandejao/ProjetoBandejao/Forms/HomeForm.cs
@@ -0,0 +1,42 @@
+﻿using ProjetoBandejao.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoBandejao.Forms
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeForm_Load(object sender, EventArgs e)
+        {
+            lblUsuario.Text =
+             UsuarioSession.UsuarioLogado.Login;
+
+            this.Shown += (s, e) =>
+            {
+                ActiveControl = null;
+            };
+        }
+
+        private void pnlFuncionarios_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnCardapio_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
