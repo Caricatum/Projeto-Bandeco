e22d7c2768db9d33d3573ef9d578d347b37916c3
Felipe Ferreira <ff.felipefer@gmail.com>
2026-06-21 14:15:42 -0300

    API: Busca cardápio por semana, de domingo a domingo

--- api/apiBandeco/src/main/java/com/example/apiBandeco/repository/CardapioDiaRepository.java
+++ api/apiBandeco/src/main/java/com/example/apiBandeco/repository/CardapioDiaRepository.java
@@ -5,11 +5,13 @@ import com.example.apiBandeco.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface CardapioDiaRepository extends JpaRepository<CardapioDia, Integer> {
     boolean existsByData(LocalDate data);
     boolean existsByDataAndIdNot(LocalDate data, int id);
     public Optional<CardapioDia> findByData(LocalDate data);
+    List<CardapioDia> findByDataBetween(LocalDate inicio, LocalDate fim);
 
 }
